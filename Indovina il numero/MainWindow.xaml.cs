1c79cecbefd58727147d9bbc707489d76e8fce9b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-09 09:48:48 +0100

    Add project files.

---
+++ Indovina il numero/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Indovina_il_numero
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        int numero_casuale;
+        private void Genera_Click(object sender, RoutedEventArgs e)
+        {
+            int livello_difficoltà = int.Parse(txtDifficoltà.Text);
+            if (livello_difficoltà < 0 || livello_difficoltà >= 101)
+                MessageBox.Show("IL NUMERO E' FUORI DAI LIMITI");
+            else
+            {
+                numero_casuale = random.Next(1, livello_difficoltà);
+                txtNumero.IsEnabled = true;
+            }
+        }
+
+        private void Indovina_Click(object sender, RoutedEventArgs e)
+        {
+            int numero = int.Parse(txtNumero.Text);
+            if (numero>numero_casuale)
+            {
+                MessageBox.Show("IL NUMERO E' FUORI DAI LIMITI");
+            } else 
+            if (numero_casuale == numero)
+                lblRisultato.Content = "Hai indovinato, Complimenti!!";
+            else if (numero_casuale < numero)
+                lblRisultato.Content = "Hai perso, il numero è più piccolo!!";
+            else if (numero_casuale > numero)
+                lblRisultato.Content = "Hai perso, il numero è più piccolo!!";
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumero.Clear();
+            txtDifficoltà.Clear();
+            lblRisultato.Content = "";
+        }
+    }
+}
